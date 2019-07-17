1ffb7e4c8c8d7bf9969557138c85118d556e9fbe
Andrew Brown <andrew.brown@intel.com>
2019-07-17 15:28:18 -0700

    Set constant offsets during relaxation (WIP)
    Need to decide whether to do this here--currently impossible due to BTreeSet--or in Function::create_constant

--- cranelift-codegen/src/binemit/relaxation.rs
+++ cranelift-codegen/src/binemit/relaxation.rs
@@ -122,7 +122,11 @@ pub fn relax_branches(func: &mut Function, isa: &dyn TargetIsa) -> CodegenResult
     let jumptables_size = offset - jumptables;
     let rodata = offset;
 
-    // TODO: Once we have constant pools we'll do some processing here to update offset.
+    // TODO we are unable to do the following because BTreeSet does not have a mutable iterator (changing a set item could impact the order so this is not allowed)
+    //    for constant in func.constants.mut_iter() {
+    //        constant.offset(offset);
+    //        offset += constant.len() as u32;
+    //    }
 
     let rodata_size = offset - rodata;
 
